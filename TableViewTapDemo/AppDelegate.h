25f5512cc3d91d42362480efa88ade483e21857a
Xummer <xummer26@gmail.com>
2013-11-19 17:45:45 +0800

    Initial Commit

---
+++ TableViewTapDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewTapDemo
+//
+//  Created by Xummer on 13-11-19.
+//  Copyright (c) 2013年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewTapDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewTapDemo
+//
+//  Created by Xummer on 13-11-19.
+//  Copyright (c) 2013年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
